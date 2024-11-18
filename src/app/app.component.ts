5b334ca3eb194e8e45fa598af211434d7cc038ad
umarshai <umarshaik119@gamil.com>
2024-11-18 11:15:31 +0530

    countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent   {
     this.getNew();
   }
   routes;
-
+ savedDate;
   title = 'LearningApp';
   getNew(){ 
     this.dbService.getData()
@@ -25,6 +25,7 @@ export class AppComponent   {
         array.push({ ...response[key] , id:key});   
       } 
     }  
+    this.savedDate = array.filter((demo)=>{ return (demo.type== 'home' && demo.question == 'targetDate')});
     array=array.filter((demo)=>{ return (demo.type== 'home' && demo.question == 'routes')});
     return array;
   }
@@ -35,7 +36,32 @@ export class AppComponent   {
     // Parse the JSON string into a JavaScript array of objects
     const dataArray = JSON.parse(jsonString);
     this.routes = dataArray;
+    this.updateCountdown(this.savedDate[0].answer);
     console.log(this.routes);
   });
+  }
+  //for countdown
+  selectedDate: string | null = null;
+  daysLeft: number | null = null;
+
+  
+
+  updateCountdown(v: Date): void {
+  
+
+    // Set the target date to December 22 of the current year
+const targetDate = new Date(v);  // Month is 0-based (11 = December)
+
+// Get today's date with time reset to 00:00:00
+const today = new Date().setHours(0, 0, 0, 0); // Reset time for accurate day difference
+
+
+const timeDifference = targetDate.getTime() - today;
+this.daysLeft = timeDifference;
+    if (timeDifference < 0) {
+      this.daysLeft = 0; // If the selected date is in the past
+    } else {
+      this.daysLeft = Math.ceil(timeDifference / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+    }
   }
 }
