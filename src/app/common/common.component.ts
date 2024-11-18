d3b860759cfa1f460fd3a85308422a323d2f003f
umarshai <umarshaik119@gamil.com>
2024-11-18 11:31:42 +0530

    change

--- src/app/common/common.component.ts
+++ src/app/common/common.component.ts
@@ -36,9 +36,38 @@ export class CommonComponent {
       } 
     }  
     array=array.filter((demo)=>{ return demo.type== route });
+    const cDate= array.filter((demo)=>{ return (demo.type== 'home' && demo.question == 'targetDate')});
+
+    this.updateCountdown(cDate[0].answer);
+
     return array;
   }
   ))
-  .subscribe((data)=>{ this.bank=data });
+  .subscribe((data)=>{ 
+    this.bank=data });
+  }
+
+  //for countdown
+  selectedDate: string | null = null;
+  daysLeft: number | null = null;
+
+
+  updateCountdown(v: Date): void {
+  
+
+    // Set the target date to December 22 of the current year
+const targetDate = new Date(v);  // Month is 0-based (11 = December)
+
+// Get today's date with time reset to 00:00:00
+const today = new Date().setHours(0, 0, 0, 0); // Reset time for accurate day difference
+
+
+const timeDifference = targetDate.getTime() - today;
+this.daysLeft = timeDifference;
+    if (timeDifference < 0) {
+      this.daysLeft = 0; // If the selected date is in the past
+    } else {
+      this.daysLeft = Math.ceil(timeDifference / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+    }
   }
 }
